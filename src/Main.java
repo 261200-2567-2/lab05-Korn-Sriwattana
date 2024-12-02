443828bd5776af004db02469fdd69b81e66b3a01
Korn Sriwattana <korn_s@cmu.ac.th>
2024-12-02 21:54:14 +0700

    ทำแลป05

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@
 public class Main {
     public static void main(String[] args) {
         // สร้างตัวละคร
-        RPGCharacter swordman = new Swordman("Tul fanPam");
-        RPGCharacter archer = new Archer("Pam fanTul");
+        RPGCharacter swordman = new Swordman("Tul");
+        RPGCharacter archer = new Archer("Pam");
 
         // สร้างอุปกรณ์เสริม
         Accessory shield = new ProtectiveAccessory("Shield", 5);
